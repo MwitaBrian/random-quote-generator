e2d000aeee255734c1dad432addc1e80797956f2
MwitaBrian <brianmwita13@gmail.com>
2022-04-13 12:08:18 +0300

    console.log to display quotes Array returned by calling getQuote function

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const getQuote = async () => {
   const res = await fetch('https://type.fit/api/quotes')
-  const quotes= await res.json();
+  const quotes = await res.json();
+  console.log(quotes);
 }
+
+getQuote();
