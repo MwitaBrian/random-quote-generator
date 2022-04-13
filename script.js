ef6646ed02bf194acd2bd71995a650d0d3f4e8fa
MwitaBrian <brianmwita13@gmail.com>
2022-04-13 12:52:23 +0300

    Use Math.floor to make num variable an integer

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const getQuote = async () => {
   const res = await fetch('https://type.fit/api/quotes')
   const quotes = await res.json();
-  const num = Math.random()*quotes.length;
+  const num = Math.floor(Math.random()*quotes.length);
   console.log(num);
 }
 
