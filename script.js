2d3ff0f7e77531d537151756aa8905120a93cce1
MwitaBrian <brianmwita13@gmail.com>
2022-04-13 12:57:31 +0300

    Access a random quote and store in item variable

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const getQuote = async () => {
   const res = await fetch('https://type.fit/api/quotes')
   const quotes = await res.json();
   const num = Math.floor(Math.random()*quotes.length);
-  console.log(num);
+  const item = quotes[num];
+  console.log(item);
 }
 
 getQuote();
